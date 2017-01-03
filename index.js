313d5be4ee5dec2f7d07d642fbd1157606e1328c
rayGreycloud <rayGreycloud@gmail.com>
2017-01-03 10:42:00 -0800

    Add populateList to create list html

--- index.js
+++ index.js
@@ -17,8 +17,24 @@ function addItem(e) {
   }
   // Add input item to items array
   items.push(item);
+  // Populate list element with items
+  populateList(items, itemsList);
   // Clear form
   this.reset();
 }
+
+function populateList(plates = [], platesList) {
+  // Map over plates and create li for each
+  platesList.innerHTML = plates.map((plate, i) => {
+    return `
+      <li>
+        <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''}/>
+        <label for="item${i}">${plate.text}</label>
+      </li>
+    `;
+    // Create one string of li's
+  }).join('');
+}
+
 // Listen for submit not click
 addItems.addEventListener('submit', addItem);
